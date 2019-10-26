f39dc140fdafcaa31e4ff498b45406e6fdec9b33
admin <admin@DESKTOP-0K8LBFF>
2019-10-26 13:40:27 +0900

    [git delete cache]
    Signed-off-by: admin <admin@DESKTOP-0K8LBFF>

--- src/basic/bruteforce/recursive/Resign.java
+++ src/basic/bruteforce/recursive/Resign.java
@@ -3,4 +3,13 @@ package basic.bruteforce.recursive;
 import java.util.Scanner;
 
 public class Resign {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] t = new int[n];
+		int[] p = new int[n];
+		while(n-->0){
+			System.out.println("haha");
+		}
+	}
 }
