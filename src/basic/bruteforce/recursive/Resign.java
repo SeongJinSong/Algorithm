586e26c824de29d9da2fe5c7de2ba5e711600b4d
admin <admin@DESKTOP-0K8LBFF>
2019-11-04 20:27:28 +0900

    [커밋]
    Signed-off-by: admin <admin@DESKTOP-0K8LBFF>

--- src/basic/bruteforce/recursive/Resign.java
+++ src/basic/bruteforce/recursive/Resign.java
@@ -3,14 +3,23 @@ package basic.bruteforce.recursive;
 import java.util.Scanner;
 
 public class Resign {
+	static int n;
+	static int[] t;
+	static int[] p;
+	static int res = 0;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] t = new int[n];
-		int[] p = new int[n];
-		while(n-->0){
-			System.out.println("haha"); 
+		n = sc.nextInt();
+		t = new int[n+1];
+		p = new int[n+1];
+		for(int i = 1 ;i < n+1 ; i++){
+			t[i] = sc.nextInt();
+			p[i] = sc.nextInt();
 		}
-		System.out.println("±ê Å×½ºÆ®");
+		
+		System.out.println(res);
+		sc.close();
+	}
+	public static void go(int day, int sum) {
 	}
 }
