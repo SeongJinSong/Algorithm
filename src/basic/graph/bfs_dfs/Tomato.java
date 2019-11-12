43d3890c82646028bcc788ca11daac393fe24f8b
sjshi <sjshi@DESKTOP-TI47F0L>
2019-11-12 21:17:57 +0900

    [tomato]진행중
    - maze, countisland 끝
    
    Signed-off-by: sjshi <sjshi@DESKTOP-TI47F0L>

--- src/basic/graph/bfs_dfs/Tomato.java
+++ src/basic/graph/bfs_dfs/Tomato.java
@@ -1,5 +1,22 @@
 package basic.graph.bfs_dfs;
 
-public class Tomato {
+import java.util.Scanner;
 
+public class Tomato {
+	static int a[][];
+	static int c[][];
+	static int dy[] = {-1, 1, 0, 0};
+	static int dx[] = { 0, 0, -1, 1};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		a = new int[n][m];
+		c = new int[n][m];
+		for(int i=n;i<n;i++) {
+			for(int j=m;j<m;j++) {
+				a[n][m] = sc.nextInt();
+			}
+		}
+	}
 }
