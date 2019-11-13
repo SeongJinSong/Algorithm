62d95abd2d3ff8641411613ff42b051dd9b554b4
sjshi <sjshi@DESKTOP-TI47F0L>
2019-11-13 18:44:44 +0900

    [숨바꼭질 완]이모티콘 진행중
    Signed-off-by: sjshi <sjshi@DESKTOP-TI47F0L>

--- src/basic/graph/bfs_dfs/Emoticon.java
+++ src/basic/graph/bfs_dfs/Emoticon.java
@@ -1,5 +1,21 @@
 package basic.graph.bfs_dfs;
 
-public class Emoticon {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class Emoticon {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		int dist[] = new int[1001];
+		int clip = 0;
+		Queue q = new LinkedList<Integer>();
+		q.add(1);
+		dist[1]=1;
+		
+		while(!q.isEmpty()) {
+			
+		}
+	}
 }
