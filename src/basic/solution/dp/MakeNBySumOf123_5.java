10efe32d6231472f4eb19140015cca39c61ec76d
admin <admin@DESKTOP-0K8LBFF>
2019-11-20 01:35:38 +0900

    [dp-study]
    Signed-off-by: admin <admin@DESKTOP-0K8LBFF>

--- src/basic/solution/dp/MakeNBySumOf123_5.java
+++ src/basic/solution/dp/MakeNBySumOf123_5.java
@@ -1,24 +1,39 @@
 package basic.solution.dp;
 
 import java.util.*;
-import java.math.*;
-
 public class MakeNBySumOf123_5 {
+    static final long mod = 1000000009L;
+    static final int limit = 100000;
+    static long[][] d = new long[limit+1][4];
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int[] d = new int[11];
-        d[0] = 1;
-        for (int i=1; i<=10; i++) {
-            for (int j=1; j<=3; j++) {
-                if (i-j >= 0) {
-                    d[i] += d[i-j];
+        for (int i=1; i<=limit; i++) {
+            if (i-1 >= 0) {
+                d[i][1] = d[i-1][2] + d[i-1][3];
+                if (i == 1) {
+                    d[i][1] = 1;
                 }
             }
+            if (i-2 >= 0) {
+                d[i][2] = d[i-2][1] + d[i-2][3];
+                if (i == 2) {
+                    d[i][2] = 1;
+                }
+            }
+            if (i-3 >= 0) {
+                d[i][3] = d[i-3][1] + d[i-3][2];
+                if (i == 3) {
+                    d[i][3] = 1;
+                }
+            }
+            d[i][1] %= mod;
+            d[i][2] %= mod;
+            d[i][3] %= mod;
         }
         int t = sc.nextInt();
         while (t-- > 0) {
             int n = sc.nextInt();
-            System.out.println(d[n]);
+            System.out.println((d[n][1] + d[n][2] + d[n][3])%mod);
         }
     }
 }
