d8f0060adaecf96166dd8d489f4a8dd64b3c226e
sjshi <sjshi@DESKTOP-3MKF4DD>
2020-01-06 17:32:41 +0900

    [dp1 문제풀이2]
    Signed-off-by: sjshi <sjshi@DESKTOP-3MKF4DD>

---
+++ src/dynamic/dp1/EatWine2.java
@@ -0,0 +1,32 @@
+package dynamic.dp1;
+
+import java.util.*;
+public class EatWine2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int d[][] = new int[n+1][3];
+    	int a[] = new int[n+1];
+    	for(int i=1;i<=n;i++) {
+    		a[i] = sc.nextInt();
+    	}
+    	d[1][1] = a[1];
+    	if(n>=2) {
+    		d[2][1] = a[2];
+    		d[2][2] = a[1]+a[2];
+    	}
+    	for(int i=3;i<=n;i++) {
+    		d[i][0] = Math.max(Math.max(d[i-1][0], d[i-1][1]), d[i-1][2]);
+    		d[i][1] = Math.max(Math.max(d[i-2][0], d[i-2][1]), d[i-2][2]) + a[i];
+    		d[i][2] = Math.max(Math.max(d[i-3][0], d[i-3][1]),  d[i-3][2]) + a[i-1] + a[i];
+    	}
+    	int ans = d[1][1];
+    	for(int i=2;i<=n;i++) {
+    		for(int j=0;j<3;j++) {
+    			ans = Math.max(ans, d[i][j]);
+    		}
+    	}
+    	System.out.println(ans);
+    	sc.close();
+	}
+}
--- src/dynamic/dp1/MostLongDecreaseSequenceSubSet_1.java
+++ src/dynamic/dp1/MostLongDecreaseSequenceSubSet_1.java
@@ -3,5 +3,25 @@ package dynamic.dp1;
 import java.util.*;
 public class MostLongDecreaseSequenceSubSet_1 {
     public static void main(String args[]) {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int a[] = new int[n+1];
+    	int d[] = new int[n+1];
+    	for(int i=1;i<=n;i++) {
+    		a[i] = sc.nextInt();
+    		d[i] = 1;
+    	}
+    	for(int i=1;i<=n;i++) {
+    		for(int j=1;j<i;j++) {
+    			if(d[i] < d[j]+1 && a[i] < a[j]) {
+    				d[i] = d[j]+1;
+    			}
+    		}
+    	}
+    	int ans = d[1];
+    	for(int i=2;i<=n;i++) {
+    		ans = Math.max(ans, d[i]);
+    	}
+    	System.out.println(ans);
     }
 }
