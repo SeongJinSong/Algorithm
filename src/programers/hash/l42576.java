656c52daf58a0c8540f2ba968673c00d868618c1
seongjin_song <seongjin_song@NB>
2021-08-21 15:36:26 +0900

    add (42576) complete

--- src/programers/hash/l42576.java
+++ src/programers/hash/l42576.java
@@ -1,27 +1,26 @@
 package programers.hash;
 
-import java.util.HashMap;
+	import java.util.HashMap;
 
 public class l42576 {
 	public static void main(String[] args) {
-		String [] participant = {"leo", "kiki", "eden"};
-		String [] competion= {"eden", "kiki"};
+		String [] participant = {"mislav", "stanko", "mislav", "ana"};
+		String [] competion= {"stanko", "ana", "mislav"};
 		
 		System.out.println(solution(participant, competion));
 	}
 	public static String solution(String[] participant, String[] completion) {
-		
 		HashMap<String, Integer> par = new HashMap<String, Integer>();
-		
-        
         for(int i=0;i<participant.length;i++){
-        	par.put(participant[i], 1);
+        	par.put(participant[i], par.getOrDefault(participant[i],0)+1);
         }
-        
         for(int i=0;i<completion.length;i++) {
-        	par.remove(completion[i]);
+        	par.put(completion[i], par.get(completion[i])-1);
         }
-        
-        return par.keySet().toString();
+        for(String key : par.keySet())
+        {
+        	if(par.get(key)!=0)return key;
+        }
+        return "error";
     }
 }
