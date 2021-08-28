4c4326cfca049cc334099862a0a8f186e69d17db
seongjin_song <seongjin_song@tmax.co.kr>
2021-08-28 13:04:38 +0900

    add : encoding convert
    Signed-off-by: seongjin_song <seongjin_song@tmax.co.kr>

---
+++ src/test/FileCharsetUtil.java
@@ -0,0 +1,88 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+
+public class FileCharsetUtil {
+	
+	public static void main(String[] args) {
+		System.out.println("=============== convert start ===============");
+		getFileList("C:/Users/admin/git/Algorithm/src", 0);		
+		System.out.println("=============== convert end ===============");
+	}
+	
+	public static void getFileList(String rootpath, int depth) {
+		String indent = "";
+		for(int i=0;i<depth;i++) indent=indent.concat(" ");
+		System.out.println(indent+"- rootpath:"+rootpath);
+		File path = new File(rootpath);
+		FileFilter javafilter = new FileFilter() {
+		    public boolean accept(File f) {
+		        return f.getName().endsWith("java");
+		    }
+		};
+		FileFilter dirfilter = new FileFilter() {
+		    public boolean accept(File f) {
+		        return !f.getName().endsWith("java");
+		    }
+		};
+		File[] javalist = path.listFiles(javafilter);
+		for(File f : javalist) {
+			System.out.println(indent+"�꽩 javaname:"+f.getName());
+			try {
+				convertEncoding(f.getAbsolutePath(), f.getAbsolutePath());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		File[] dirlist = path.listFiles(dirfilter);
+		for(File f : dirlist) {
+			System.out.println(indent+"dirname:"+f.getName());
+			getFileList(f.getAbsolutePath(), depth+1);
+		}
+	}
+	 
+    /**
+     * UTF-8 encoding �뙆�씪�쓣 MS949 encoding �뙆�씪濡� 蹂�寃�
+     * @param inFileName UTF-8 �뙆�씪 議댁옱�쐞移�
+     * @param outFileName �깉濡� �깮�꽦�맆 MS949 �뙆�씪 ���옣�쐞移�
+     * @throws Exception
+     */
+    public static void convertEncoding(String inFileName, String outFileName) throws Exception {
+         
+        // ================================
+        FileInputStream fileInputStream = null;
+        Reader reader = null;
+        Writer writer = null;
+        StringBuffer stringBuffer = new StringBuffer();
+         
+        int intRead = 0;
+         
+        fileInputStream = new FileInputStream(inFileName);
+        Charset inputCharset = Charset.forName("MS949");
+        InputStreamReader isr = new InputStreamReader(fileInputStream, inputCharset);
+         
+        reader = new BufferedReader(isr);
+         
+        while( ( intRead = reader.read() ) > -1 ) {
+            stringBuffer.append((char)intRead);
+        }
+        reader.close();
+         
+        //
+        FileOutputStream fos = new FileOutputStream(outFileName);
+        writer = new OutputStreamWriter(fos, "UTF-8");
+        writer.write(stringBuffer.toString());
+        stringBuffer.setLength(0);
+        writer.close();
+    }
+}
