cbe26ae2540a9aa49129ab1b25576f4c76704748
seongjin_song <seongjin_song@tmax.co.kr>
2021-09-01 21:18:23 +0900

    fix(l42579) : 스트림 개발 방법 추가
    - mapToObj
    - collect(Collectors.groupingBy(Class::Function))
    - sorted((a,b)-> sum(b.getValue())-sum(a.getValue()))
    - flatMap -> 그룹화를 통한 [[a][b][c]] to [a, b, c]

--- src/programers/hash/l42579.java
+++ src/programers/hash/l42579.java
@@ -3,12 +3,17 @@ package programers.hash;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class l42579 {
 	public static void main(String[] args) {
 		String[] genres = {"classic", "pop", "classic", "classic", "pop"};
 		int[] plays = {500, 600, 150, 800, 2500};
-		print(solution(genres, plays));
+		
+		//print(solution(genres, plays));
+		print(new l42579().solution2(genres, plays));
 	}
 	public static int[] solution(String[] genres, int[] plays) {
         Integer[] idx = new Integer[genres.length];
@@ -64,5 +69,42 @@ public class l42579 {
 	}
 	public static void print(int[] arr) {
 		for(int a:arr)System.out.print(a+" ");
+	}
+	
+	/* 스트림을 이용한 개발 */
+	public class Music implements Comparable<Music>{
+		private int played;
+		private int id;
+		private String genre;
+		
+		public Music(String genre, int played, int id) {
+			this.genre=genre;
+			this.played=played;
+			this.id = id;
+		}
+
+		@Override
+		public int compareTo(Music other) {
+			if(this.played == other.played) return this.id-other.id;
+			return other.played-this.played;
+		}
+		public String getGenre() {return genre;}
+	}
+	public int[] solution2(String[] genres, int[] plays) {
+		return IntStream.range(0, genres.length)
+				.mapToObj(i->new Music(genres[i], plays[i], i))
+				.collect(Collectors.groupingBy(Music::getGenre))
+				.entrySet()
+				.stream()
+				.sorted((a,b)->sum(b.getValue()) - sum(a.getValue()))
+				.flatMap(x->x.getValue().stream().sorted().limit(2))
+				.mapToInt(x->x.id).toArray();
+	}
+	private int sum(List<Music> value) {
+		int answer =0;
+		for(Music music : value) {
+			answer+=music.played;
+		}
+		return answer;
 	}
 }
