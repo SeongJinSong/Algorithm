75e9022f2e6dc5e94210cd925d7fc0e80a001d90
송성진 <seongjin_song@tmax.co.kr>
2021-09-04 14:53:18 +0900

    add(l42587) : queue문제 풀기

---
+++ src/programers/queue_stack/ㅣ42587.java
@@ -0,0 +1,36 @@
+package programers.queue_stack;
+
+import java.util.ArrayList;
+
+public class ㅣ42587 {
+	public static void main(String[] args) {
+		int[] priorities = {2, 1, 3, 2};
+		int location = 2;
+		System.out.println(solution(priorities, location));
+	}
+	public static int solution(int[] priorities, int location) {
+        ArrayList<Integer> queue = new ArrayList<Integer>();
+        for(int i=0;i<priorities.length;i++){
+        	queue.add(priorities[i]);
+        }
+        int answer = 0;
+        loop : while(location!=-1) {
+        	for(int i=1;i<queue.size();i++)
+        	{
+        		if(queue.get(0)<queue.get(i)) {
+        			int cur = queue.remove(0);
+        			queue.add(cur);
+        			if(location==0) {
+        				location=queue.size()-1;
+        			}else location--;
+        			continue loop;
+        		}
+        	}
+        	queue.remove(0);
+        	answer++;
+        	location-=1;
+        }
+        
+        return answer;
+    }
+}
