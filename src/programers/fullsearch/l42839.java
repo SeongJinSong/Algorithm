bbd50b55a54ac89a5d308873118c335143e2c1ba
seongjin_song <seongjin_song@tmax.co.kr>
2021-09-12 01:59:26 +0900

    chore : 하던거 저장

--- src/programers/fullsearch/l42839.java
+++ src/programers/fullsearch/l42839.java
@@ -4,13 +4,16 @@ import java.util.HashSet;
 
 public class l42839 {
 	public static void main(String[] args) {
-//		String numbers = "17";
-		String numbers = "011";
+		String numbers = "123";
+		//String numbers = "011";
+		//System.out.println(isPrimeNumber(11));
 		System.out.println(solution(numbers));
+		System.out.println("cnt:"+cnt);
 		
 	}
 	static HashSet<Integer> hs;
 	static String[] digit;
+	static int cnt=0;
 	
 	public static int solution(String numbers) {
 		int answer=0;
@@ -18,21 +21,25 @@ public class l42839 {
 		for (int i = 0; i < numbers.length(); i++)
 			digit[i] = numbers.charAt(i) + "";
 		hs = new HashSet<Integer>();
-		makeNumber("", 0);
+		for(int i=0;i<digit.length;i++)
+			makeNumber(digit[i], 0, i);
 		for(int s : hs) {
 			if(isPrimeNumber(s))answer++;
 		}
 		return answer;
 	}
 
-	public static void makeNumber(String cur, int depth) {
+	public static void makeNumber(String cur, int depth, int start) {
 		if(depth>digit.length-1) {
+			cnt++;
 			hs.add(StringToInt(cur));
 			return;
 		}
-		makeNumber(cur+digit[depth], depth+1);
-		makeNumber(cur, depth+1);
-		makeNumber(digit[depth]+cur, depth+1);
+		makeNumber(cur, depth+1, start);
+		if(depth!=start) {
+			makeNumber(cur+digit[depth], depth+1, start);
+			makeNumber(digit[depth]+cur, depth+1, start);
+		}
 
 	}
 	public static Integer StringToInt(String s) {
@@ -44,8 +51,9 @@ public class l42839 {
 	}
 
 	public static boolean isPrimeNumber(int n) {
+		System.out.println("n:"+n);
 		if(n<2) return false;
-		for (int i = 2; i <= n / 2; i++) {
+		for (int i = 2; i * i<= n; i++) {
 			if (n % i == 0)
 				return false;
 		}
