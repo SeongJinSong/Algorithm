25003e98e714b02cf82d78d6b9d7a2b31232ef84
seongjin_song <seongjin_song@tmax.co.kr>
2021-10-13 23:36:37 +0900

    add(l60057) : 다른 솔루션 추가

--- src/programers/lv2/l60057.java
+++ src/programers/lv2/l60057.java
@@ -5,9 +5,9 @@ public class l60057 {
 //		String s="aabbaccc";
 //		String s="ababcdcdababcdcd";
 //		String s="abcabcdede";
-//		String s="abcabcabcabcdededededede";
-		String s="xababcdcdababcdcd";
-		System.out.println(solution(s));
+		String s="abcabcabcabcdededededede";
+//		String s="xababcdcdababcdcd";
+		System.out.println(solution3(s));
 	}
 	public static int solution(String s) {
         int answer = s.length();
@@ -38,8 +38,8 @@ public class l60057 {
         				cnt=1;
         			}
         			else {
-        				sb.append(s.charAt(curIdx));
-        				curIdx++;
+        				sb.append(cur);
+        				curIdx+=size;
         				appendIdx=curIdx;
         			}
         			
@@ -53,4 +53,64 @@ public class l60057 {
         }
         return answer;
     }
+	/* 재귀호출 방식을 이용하여 푼 예
+	 *  - 1개도 일치하지 않은 경우 prestring 붙이고
+	 *  - 1개 이상 일치했던 경우는 숫자 + preString 붙이고
+	 *  - 일치하는 경우는 split한다.
+	 * */
+	public static int solution2(String s) {
+		int answer = 0;
+		for(int i=1;i<=(s.length()/2)+1;i++) {
+			int result = getSplitedLength(s, i, 1).length();
+			answer = i==1?result:(answer>result?result:answer); // 최소값 비교를 한줄로 하는 방법
+		}
+		return answer;
+	}
+	public static String getSplitedLength(String s, int n, int repeat) {
+		if(s.length()<n) return s;
+		String result = "";
+		String preString = s.substring(0, n);
+		String postString = s.substring(n, s.length());
+		
+		//불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+		if(!postString.startsWith(preString)) {
+			if(repeat==1)return result+=preString + getSplitedLength(postString, n, 1);
+			return result+=repeat+preString+getSplitedLength(postString, n, repeat+1);
+		}
+		return result+=getSplitedLength(postString, n, repeat+1);
+	}
+	
+	/* 내가 푼 방법과 플이 원칙은 같지만 자릿수만 계산하는 풀이라서 내 풀이보다 조금더 깔끔하다.
+	 * Math.log10(cnt)을 통해 자릿수만 계산
+	 * */
+	public static int solution3(String s) {
+		int min = s.length();
+		int len = s.length()/2+1;
+		for(int size=1;size<len;size++) {
+			String before = "";
+			int sum = 0;
+			int cnt = 1;
+			for(int j=0;j<s.length();) {
+				int start = j;
+				j = (j+size>s.length())?s.length():j+size;
+				String temp = s.substring(start, j);
+				if(temp.equals(before)) {
+					cnt++;
+				}else {
+					if(cnt!=1) {
+						sum+=(int)Math.log10(cnt)+1;
+					}
+					cnt=1;
+					sum+=before.length();
+					before=temp;
+				}
+			}
+			sum+=before.length();
+			if(cnt!=1) {
+				sum+=(int)Math.log10(cnt)+1;
+			}
+			min = (min>sum)?sum:min;
+		}
+		return min;
+	}
 }
