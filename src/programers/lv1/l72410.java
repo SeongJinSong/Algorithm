66a6ee310ec084aac02f043e09571b5ec16f70fc
seongjin_song <seongjin_song@tmax.co.kr>
2021-10-19 23:12:40 +0900

    add(l72410) : 솔루션 추가

--- src/programers/lv1/l72410.java
+++ src/programers/lv1/l72410.java
@@ -84,4 +84,79 @@ public class l72410 {
 		}
 		return sb.toString();
 	}
+	/* 정규표현식을 사용한 풀이 추가	
+	 * 막상 이게 더 실수가 많을 것 같아 불안해서 직접 했는데 막상 보니 이게 더 쉬운것 같다.
+	 * 비슷한 유형인 경우 정규표현식으로 풀도록 하자*/
+	public String solution2(String new_id) {
+		String temp = new_id.toLowerCase();
+		
+		temp = temp.replaceAll("[^-_.a-z0-9]", "");
+		temp = temp.replaceAll("[.]{2,}", ".");
+		temp = temp.replaceAll("^[.]|[.]$", "");
+		if(temp.equals(""))temp+="a";
+		if(temp.length()>=16) {
+			temp=temp.substring(0,15);
+			temp=temp.replaceAll("^[.]|[.]$","");
+		}
+		if(temp.length()<=2)
+			while(temp.length()<3)
+				temp+=temp.charAt(temp.length()-1);
+		return temp;
+	}
+	/*자바스럽게 구현하기*/
+	public String solution3(String new_id) {
+		return new KAKAOID(new_id)
+					.replaceToLowerCase()
+					.filter()
+					.toSingleDot()
+					.noStartEndDot()
+					.noBlank()
+					.noGreaterThan16()
+					.noLessThan2()
+					.getResult();
+	}
+	public static class KAKAOID{
+		private String s;
+		public KAKAOID(String s) {
+			this.s=s;
+		}
+		private KAKAOID replaceToLowerCase() {
+			s=s.toLowerCase();
+			return this;
+		}
+		private KAKAOID filter() {
+			s = s.replaceAll("[^a-z0-9._-]", "");
+			return this;
+		}
+		private KAKAOID toSingleDot() {
+            s = s.replaceAll("[.]{2,}", ".");
+            return this;
+        }
+		private KAKAOID noStartEndDot() {
+            s = s.replaceAll("^[.]|[.]$", "");
+            return this;
+        }
+		private KAKAOID noBlank() {
+            s = s.isEmpty() ? "a" : s;
+            return this;
+        }
+		private KAKAOID noGreaterThan16() {
+            if (s.length() >= 16) {
+                s = s.substring(0, 15);
+            }
+            s = s.replaceAll("[.]$", "");
+            return this;
+        }
+		private KAKAOID noLessThan2() {
+            StringBuilder sb = new StringBuilder(s);
+            while (sb.length() <= 2) {
+                sb.append(sb.charAt(sb.length() - 1));
+            }
+            s=sb.toString();
+            return this;
+        }
+		private String getResult() {
+            return s;
+        }
+	}
 }
