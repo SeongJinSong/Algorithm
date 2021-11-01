de54825333057a803baea558139a6a9642e23760
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-01 17:57:17 +0900

    fix(l1830) : 문제 푸는중

--- src/programers/lv3/l1830.java
+++ src/programers/lv3/l1830.java
@@ -7,11 +7,26 @@ public class l1830 {
 //		String sentence = "HaEaLaLaObWORLDb";
 //		String sentence = "SpIpGpOpNpGJqOqA";
 //		String sentence = "AxAxAxAoBoBoB";
-		String sentence = "AbAaAbAaCa";
+//		String sentence = "AbAaAbAaCa";
+//		String sentence = "aIaAM";
+//		String sentence = "AAAaBaAbBBBBbCcBdBdBdBcCeBfBeGgGGjGjGRvRvRvRvRvR";
+//		String sentence = "aaA";
+//		String sentence = "Aaa";
+//		String sentence = "aHELLOWORLDa";
+//		String sentence = "HaEaLaLObWORLDb";
+//		String sentence = "HaEaLaLaObWORLDb";
+//		String sentence = "aHbEbLbLbOacWdOdRdLdDc";
+//		String sentence = "abAba";
+//		String sentence = "HELLO WORLD";
+//		String sentence = "xAaAbAaAx"; // rule2 중복 체크
+//		String sentence = "AbAaAbAaCa";
+		String sentence = "AbAaAbAaC"; // rule2 중복 체크
 //		System.out.println(new l1830().restoreRule2(sentence, 'a'));
 		System.out.println(new l1830().solution(sentence));
 		
 	}
+	String err = "invalid";
+	HashMap<Integer, String> hm2;
 	public String solution(String sentence) {
 		/* 1. 띄어쓰기 case insert
 		 * 규칙 2 확인
@@ -34,16 +49,18 @@ public class l1830 {
 		 *   
 		 */ 
 		String tmp = sentence;
-		String err = "invalid";
+		if(sentence.contains(" "))return err;
 		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
 		for(int i=0;i<sentence.length();i++) {
 			if(checkmini(sentence.charAt(i))){
 				hm.put((int)sentence.charAt(i), hm.getOrDefault((int)sentence.charAt(i), 0)+1);
 			}
 		}
+		hm2 = new HashMap<Integer, String>();
 		for(int i : hm.keySet()) {
 			if(hm.get(i)==2) {
 				tmp=restoreRule2(tmp, (char)i);
+				if(err.equals(tmp))return err;
 			}
 		}
 //		System.out.println("1 tmp:"+tmp);
@@ -78,9 +95,10 @@ public class l1830 {
 		int start = sentence.indexOf(c);
 		int end = sentence.lastIndexOf(c)+1;
 		boolean toggle = true;
-		String err = "invalid";
 		StringBuilder sb = new StringBuilder();
-		if(' '==sentence.charAt(start-1))return err;
+		System.out.println("start:"+start+"sentence.charAt(start-1):"+sentence.charAt(start-1));
+		if(start>0&&' '==sentence.charAt(start-1))return err;
+		
 		for(int i=start; i<=end;i++) {
 			if(toggle&&c==sentence.charAt(i)) {
 				toggle=!toggle;
@@ -95,17 +113,32 @@ public class l1830 {
 			}
 		}
 //		System.out.println("end:"+end+" sentence.len:"+sentence.length());
-		return sentence.substring(0, start)
+		String s = sentence.substring(0, start-1)
+				+((start>1&&' '!=sentence.charAt(start-2))?" ":"")
+				+sentence.charAt(start-1)
 				+sb.toString()
 				+(end==sentence.length()-1||' '==sentence.charAt(end+1)?"":" ")
 				+sentence.substring(end+1, sentence.length());
+		System.out.println("s:"+s);
+		return s;
 	}
 	public String restoreRule2(String sentence, char c) {
 		int start = sentence.indexOf(c);
 		int end = sentence.lastIndexOf(c);
+		if(hm2.isEmpty())hm2.put((int)c, start+" "+end);
+		else {
+			for(String s:hm2.values()) {
+				int tmpStart = Integer.valueOf(s.split(" ")[0]);
+				int tmpEnd = Integer.valueOf(s.split(" ")[1]);
+				if(tmpStart>end||tmpEnd<start)hm2.put((int)c, start+" "+end);
+				else return err;
+			}
+		}
+		if(sentence.substring(start+1, end-1).matches("^[a-z]*$"))return err;
+		if(start==end-1)return err;
 		StringBuilder sb = new StringBuilder();
 		sb.append(sentence.substring(0, start));
-		if(' '!=sentence.charAt(start-1))sb.append(' ');
+		if(start>0&&' '!=sentence.charAt(start-1))sb.append(' ');
 		sb.append(sentence.substring(start+1, end));
 		if(end!=sentence.length()-1)sb.append(" ");
 		sb.append(sentence.substring(end+1, sentence.length()));
