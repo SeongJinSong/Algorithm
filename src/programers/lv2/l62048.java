5fa60a8d5259f10263b5a694ce10638c971076b6
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-09 23:01:03 +0900

    complete(l62048)
    1. 최소 직사각형으로 푸는 경우
    minW+minM-1 (가로와 세로 동시에 통과하는 부분이 1개이다.)
    
    2. BigInteger에 GCD 함수가 있는걸 첨 알았다. 유용하게 쓸 수 있을 것 같다.
    대각선의 길이  =가로 + 세로 - 가로세로의 최대공약수

--- src/programers/lv2/l62048.java
+++ src/programers/lv2/l62048.java
@@ -1,5 +1,7 @@
 package programers.lv2;
 
+import java.math.BigInteger;
+
 public class l62048 {
 	public static void main(String[] args) {
 		int w=8, h=12;
@@ -18,18 +20,29 @@ public class l62048 {
 		long minW = w/val;
 		long minH = h/val;
 		if(minW==1||minH==1)return w*h-val*minW*minH;
-		long cnt = 0;
-        for(int i=0;i<minW;i++) {
-        	for(int j=0;j<minH;j++) {
-        		if(j*minW>minH*(i+1))continue;
-        		if(i!=0&&(j+1)*minW<minH*i)continue;
-        		cnt++;
-        	}
-        }
+		/*패턴 직사각형이 다음 꼭지점으로 도달하기 위해서는 가로 길이, 세로 길이만큼 움직여야 하는데
+		 * 가로 길이, 세로 길이중 겹치는 부분이 있기 때문에 1을 빼는 것이다.*/
+		long cnt = minW+minH-1;
+		
+		/* 성능 테스트 통과 못함        
+		for(int i=0;i<minW;i++) {
+			for(int j=0;j<minH;j++) {
+				if(j*minW>minH*(i+1))continue;
+				if(i!=0&&(j+1)*minW<minH*i)continue;
+				cnt++;
+			}
+		}*/
         return w*h-val*cnt;
     }
 	public long gcd(long a, long b) {
 		if(b==0)return a;
 		else return gcd(b, a%b);
+	}
+	/*BigInteger에 GCD 함수가 있는걸 첨 알았다. 유용하게 쓸 수 있을 것 같다.
+	 * 대각선의 길이는 가로 + 세로 - 가로세로의 최대공약수*/
+	public long solution2(int w, int h) {
+		long totalCount = (long)w * (long)h;
+		long diagonalCount = w+h-BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).longValue();
+		return totalCount-diagonalCount;
 	}
 }
