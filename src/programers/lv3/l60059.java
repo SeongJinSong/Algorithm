2f7d69f567492e13fb51b1d24d33d32d300b3cdb
송성진 <seongjin_song@tmax.co.kr>
2021-11-11 12:49:54 +0900

    add(l60059)

---
+++ src/programers/lv3/l60059.java
@@ -0,0 +1,11 @@
+package programers.lv3;
+
+public class l60059 {
+	public static void main(String[] args) {
+		
+	}
+	public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        return answer;
+    }
+}
