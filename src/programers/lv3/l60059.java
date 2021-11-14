7c951f6b96c42646e56dfbe103c66a744a8f7bb6
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-15 00:54:49 +0900

    add(l60059)
    - lock을 key배열로 가장자리을 따라 이동할 수 있는 크기의 bigArr 선언
    - bigArr 가운데 부분을 lock 데이터 넣고 초기화 배열로 만듬
    - key가 가장자리를 순회해서 값을 더함
    - lock 부분이 2 또는 0이 된다면 오류
    
    (): 회전하는 함수 구현, 유효성 체크 함수 구현, addKey 함수 구현

--- src/programers/lv3/l60059.java
+++ src/programers/lv3/l60059.java
@@ -2,10 +2,58 @@ package programers.lv3;
 
 public class l60059 {
 	public static void main(String[] args) {
-		
+		int[][] key= {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}; int[][] lock= {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+		System.out.println(new l60059().solution(key, lock));
 	}
 	public boolean solution(int[][] key, int[][] lock) {
-        boolean answer = true;
-        return answer;
+		int kl = key.length;
+		int ll = lock.length;
+		int[][] OrgBigArr = new int[2*(kl-1)+ll][2*(kl-1)+ll];
+		for(int i=0;i<ll;i++) {
+			for(int j=0;j<ll;j++) {
+				OrgBigArr[kl-1+i][kl-1+j] = lock[i][j]; 
+			}
+		}
+		
+		for(int r=0;r<4;r++) {
+			if(r==0)continue;
+			else key = rotation90(key);
+			for(int i=0;i<OrgBigArr.length-kl;i++) {
+				for(int j=0;j<OrgBigArr.length-kl;j++) {
+					int[][] tmpBigArr = OrgBigArr.clone();
+					addkey(tmpBigArr, key, i, j);
+					if(checkfit(tmpBigArr, ll))return true;
+				}
+			}
+			
+		}
+        return false;
     }
+	public void addkey(int[][] arr, int[][] key, int offsetX, int offsetY) {
+		for(int i=0;i<key.length;i++) {
+			for(int j=0;j<key.length;j++) {
+				arr[i+offsetX][j+offsetY]+=key[i][j];
+			}
+		}
+	}
+	public boolean checkfit(int[][] arr, int ll) {
+		for(int i=0;i<ll;i++) {
+			for(int j=0;j<ll;j++) {
+				if(arr[ll-1+i][ll-1+j]==2||arr[ll-1+i][ll-1+j]==0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public int[][] rotation90(int[][] key) {
+		int kl = key.length;
+		int[][] rotationKey = new int[kl][kl];
+		for(int i=0;i<kl;i++) {
+			for(int j=0;j<kl;j++) {
+				rotationKey[i][j]=key[j][kl-1-i];
+			}
+		}
+		return rotationKey;
+	}
 }
