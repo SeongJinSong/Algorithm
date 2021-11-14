1cb5d519d9eae194b954725eee32716f16c5e381
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-15 01:21:38 +0900

    fix(l60059)
    - int[][] array clone 함수는 오류가 있다.

--- src/programers/lv3/l60059.java
+++ src/programers/lv3/l60059.java
@@ -3,6 +3,7 @@ package programers.lv3;
 public class l60059 {
 	public static void main(String[] args) {
 		int[][] key= {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}; int[][] lock= {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+//		int[][] key= {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; int[][] lock= {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 		System.out.println(new l60059().solution(key, lock));
 	}
 	public boolean solution(int[][] key, int[][] lock) {
@@ -15,20 +16,28 @@ public class l60059 {
 			}
 		}
 		
+		int[][] tmpBigArr = new int[OrgBigArr.length][OrgBigArr.length];
 		for(int r=0;r<4;r++) {
 			if(r==0)continue;
 			else key = rotation90(key);
 			for(int i=0;i<OrgBigArr.length-kl;i++) {
 				for(int j=0;j<OrgBigArr.length-kl;j++) {
-					int[][] tmpBigArr = OrgBigArr.clone();
+					initArr(tmpBigArr, OrgBigArr);
 					addkey(tmpBigArr, key, i, j);
-					if(checkfit(tmpBigArr, ll))return true;
+					if(checkfit(tmpBigArr, kl, ll))return true;
 				}
 			}
 			
 		}
         return false;
     }
+	public void initArr(int[][] tmpBigArr, int[][] OrgBigArr) {
+		for(int i=0;i<OrgBigArr.length;i++) {
+			for(int j=0;j<OrgBigArr.length;j++) {
+				tmpBigArr[i][j]=OrgBigArr[i][j];
+			}
+		}
+	}
 	public void addkey(int[][] arr, int[][] key, int offsetX, int offsetY) {
 		for(int i=0;i<key.length;i++) {
 			for(int j=0;j<key.length;j++) {
@@ -36,10 +45,10 @@ public class l60059 {
 			}
 		}
 	}
-	public boolean checkfit(int[][] arr, int ll) {
+	public boolean checkfit(int[][] arr, int kl, int ll) {
 		for(int i=0;i<ll;i++) {
 			for(int j=0;j<ll;j++) {
-				if(arr[ll-1+i][ll-1+j]==2||arr[ll-1+i][ll-1+j]==0) {
+				if(arr[kl-1+i][kl-1+j]!=1) {
 					return false;
 				}
 			}
