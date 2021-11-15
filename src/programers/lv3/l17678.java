a9fa64d0898a2b83e3bbbf612f3ea6ff982860ca
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-15 18:37:32 +0900

    add(l17678)

---
+++ src/programers/lv3/l17678.java
@@ -0,0 +1,11 @@
+package programers.lv3;
+
+public class l17678 {
+	public static void main(String[] args) {
+		
+	}
+	public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+        return answer;
+    }
+}
