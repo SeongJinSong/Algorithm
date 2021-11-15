d900739b6054eb98d82bf3769e5075eca3cbf386
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-16 00:58:16 +0900

    add(l17678)
    문제 이해하는데 어려움을 겪고 있다.
    처음 읽었을때도 한번에 이해를 못했다.
    - 2번 6번 예제 케이스가 이해가 안간다.

--- src/programers/lv3/l17678.java
+++ src/programers/lv3/l17678.java
@@ -2,7 +2,8 @@ package programers.lv3;
 
 public class l17678 {
 	public static void main(String[] args) {
-		
+		int n=1, t=1, m=5; String[] timetable= {"08:00", "08:01", "08:02", "08:03"};
+		System.out.println(new l17678().solution(n, t, m, timetable));
 	}
 	public String solution(int n, int t, int m, String[] timetable) {
         String answer = "";
