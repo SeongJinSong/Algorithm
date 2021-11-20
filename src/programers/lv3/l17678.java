839b038a6cd0a71ff916641a7b8e59915d9d3a4c
seongjin_song <seongjin_song@tmax.co.kr>
2021-11-20 19:32:45 +0900

    complete(l17678)
    - hh:mm >> HH:mm 이 문제였다.
       로컬에서 테스트시 콘솔 찍었을 땐 문제가 없어서 방심했다.

--- src/programers/lv3/l17678.java
+++ src/programers/lv3/l17678.java
@@ -14,7 +14,6 @@ public class l17678 {
 //		int n=1, t=1, m=1; String[] timetable= {"23:59"};
 //		int n=2, t=10, m=3; String[] timetable= {"09:05","09:09","09:13"};
 		int n=10, t=1, m=5; String[] timetable= {"00:00","09:00","09:00","09:00","09:00"};
-		
 		System.out.println(new l17678().solution(n, t, m, timetable));
 	}
 	public String solution(int n, int t, int m, String[] timetable) throws ParseException {
@@ -41,7 +40,7 @@ public class l17678 {
         return getHHMMTime(new Date(result).toString());
     }
 	public long getlongTime(String s) throws ParseException {
-		SimpleDateFormat format = new SimpleDateFormat("hh:mm");
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
 		return format.parse(s).getTime();
 	}
 	public String getHHMMTime(String s) {
