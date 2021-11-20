09cb4eae71f9a70fa0639784cfd9477041081604
송성진 <seongjin_song@tmax.co.kr>
2021-11-20 14:38:30 +0900

    feat(l17681) 솔루션 추가
    - or연산이라 | 만으로 처리 가능하다.(원래 알았지만 실수할까봐 직접만들었다)
    - Integer.toBinaryString 함수로 2진수 결과를 간단히 만들 수 있다.

--- src/programers/lv1/l17681.java
+++ src/programers/lv1/l17681.java
@@ -1,6 +1,14 @@
 package programers.lv1;
 
 public class l17681 {
+	public static void main(String[] args) {
+//		int n=5; int[] arr1= {9, 20, 28, 18, 11}; int[] arr2= {30, 1, 21, 17, 28};
+		int n=6; int[] arr1= {46, 33, 33 ,22, 31, 50}; int[] arr2= {27 ,56, 19, 14, 14, 10};
+		new l17681().print(new l17681().solution2(n, arr1, arr2));
+	}
+	public void print(String [] arr) {
+		for(String s : arr)System.out.println(s);
+	}
 	public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for(int i=0;i<n;i++){
@@ -30,5 +38,18 @@ public class l17681 {
             else sb.append('#');
         }
         return sb.toString();
+    }
+    public String[] solution2(int n, int[] arr1, int[] arr2) {
+    	String[] result = new String[n];
+    	for(int i=0;i<n;i++) {
+    		result[i]=Integer.toBinaryString(arr1[i]|arr2[i]);
+    	}
+    	
+    	for(int i=0;i<n;i++) {
+    		result[i]=String.format("%"+n+"s", result[i]);
+    		result[i]=result[i].replace("1", "#");
+    		result[i]=result[i].replace("0", " ");
+    	}
+    	return result;
     }
 }
