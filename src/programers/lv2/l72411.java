9b70c03894561a8bc6f206c1d29356351de1dad7
seongjin_song <seongjin_song@tmax.co.kr>
2021-12-03 23:46:10 +0900

    complete(l72411): 성능테스트 통과
    - 전체 기준으로 comb 하지않고 매뉴 각각 기준으로 comb를 만든다.

--- src/programers/lv2/l72411.java
+++ src/programers/lv2/l72411.java
@@ -1,14 +1,15 @@
 package programers.lv2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
 public class l72411 {
 	public static void main(String[] args) {
-		String[] orders= {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}; int[] course= {2,3,4};
+//		String[] orders= {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}; int[] course= {2,3,4};
 //		String[] orders= {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}; int[] course= {2,3,5};
-//		String[] orders= {"XYZ", "XWY", "WXA"}; int[] course= {2,3,4};
+		String[] orders= {"XYZ", "XWY", "WXA"}; int[] course= {2,3,4};
 		new l72411().print(new l72411().solution(orders, course));
 	}
 	class Course{
@@ -25,26 +26,25 @@ public class l72411 {
 	ArrayList<Character> a;
 	ArrayList<Course> res;
 	HashSet<String> hs;
+	String[] sortedOrders;
+	int max=0;
 	public String[] solution(String[] orders, int[] course) {
-		a = new ArrayList<Character>();
 		hs = new HashSet<String>();
-		for(String s : orders) {
-			for(int i=0;i<s.length();i++) {				
-				if(a.indexOf(s.charAt(i))==-1) {
-					a.add(s.charAt(i));
-				}
-			}
-		}
-		Collections.sort(a);
-		int n = a.size();
-		visited = new boolean[n];
 		ArrayList<String> result = new ArrayList<String>();
+		sortedOrders = new String[orders.length];
+		for(int i=0;i<orders.length;i++) {
+			char[] tmp = orders[i].toCharArray();
+			Arrays.sort(tmp);
+			StringBuilder sb = new StringBuilder();
+			for(char c : tmp)sb.append(c);
+			sortedOrders[i]=sb.toString();
+		}
 		for(int r : course) {
 			res = new ArrayList<Course>(); 
-			comb(orders, n, r, 0);
-			int max = 0;
-			for(Course nd : res) {
-				if(max<nd.cnt)max=nd.cnt;
+			max=0;
+			for(String order:sortedOrders) {
+				visited = new boolean[order.length()];
+				comb(order, order.length(), r, 0);
 			}
 			for(Course nd : res) {
 				if(max==nd.cnt)result.add(nd.sb.toString());
@@ -53,33 +53,36 @@ public class l72411 {
 		Collections.sort(result);
         return result.stream().toArray(String[]::new);
     }
-	public void comb(String[] orders, int n, int r, int depth) {
+	public void addRes(String order,  int n, int r, StringBuilder sb) {
+		int cnt=0;
+		for(int i=0;i<sortedOrders.length;i++) {
+			int len=0;
+			for(int j=0;j<n;j++) {
+				if(visited[j]&&sortedOrders[i].lastIndexOf(order.charAt(j))!=-1) {
+					len++;
+				}
+			}
+			if(len==r)cnt++;
+		}
+		if(cnt>=max&&cnt>1) {
+			max=cnt;
+			res.add(new Course(sb, r, cnt));
+		}
+	}
+	public void comb(String order, int n, int r, int depth) {
 		if(depth==r) {
 			StringBuilder sb = new StringBuilder();
-			int cnt=0;
 			for(int i=0;i<visited.length;i++) {
-				if(visited[i])sb.append(a.get(i));
+				if(visited[i])sb.append(order.charAt(i));
 			}
-			if(hs.contains(sb.toString()))return;
-			for(int i=0;i<orders.length;i++) {
-				int len=0;
-				for(int j=0;j<n;j++) {
-					if(visited[j]&&orders[i].lastIndexOf(a.get(j))!=-1) {
-						len++;
-					}
-				}
-				if(len==r)cnt++;
-			}
-			if(cnt>1) {
-				hs.add(sb.toString());
-				res.add(new Course(sb, r, cnt));
-			}
+			if(!hs.add(sb.toString()))return;
+			addRes(order, n, r, sb);
 			return;
 		}
-		for(int i=0;i<n;i++) {
+		for(int i=depth;i<n;i++) {
 			if(!visited[i]) {
 				visited[i]=true;
-				comb(orders, n, r, depth+1);
+				comb(order, n, r, depth+1);
 				visited[i]=false;
 			}
 		}
