6c3866bab17150043a5a8be7a2f41bc68dc03c3c
seongjin_song <seongjin_song@tmax.co.kr>
2021-12-04 19:45:07 +0900

    complete(l12925)

---
+++ src/programers/lv1/l12925.java
@@ -0,0 +1,12 @@
+package programers.lv1;
+
+public class l12925 {
+	public static void main(String[] args) {
+//		String s="1234";
+		String s="-1234";
+		System.out.println(new l12925().solution(s));
+	}
+	public int solution(String s) {
+        return Integer.valueOf(s);
+    }
+}
