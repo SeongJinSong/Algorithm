bc70d0e9ff5e8ad11e893b0883244e1aca5dbc38
seongjin_song <seongjin_song@tmax.co.kr>
2021-12-22 00:32:49 +0900

    complete(sutdy3-num1)
    - sliding window 기법에 대해 배움
    - 각 단계별 시간복잡도와 공간복잡도에 대해 배움

--- src/test/study3/num1.java
+++ src/test/study3/num1.java
@@ -1,6 +1,8 @@
 package test.study3;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class num1 {
 	//문자열 substring 문제
@@ -10,8 +12,9 @@ public class num1 {
 	
 	// 가장 긴 substring의 길이를 단순히 리턴하는 로직을 짜는 것 보다 최대한 효율적인 알고리즘을 생각해내는게 더 중요 
 	public static void main(String[] args) {
-		String s = "abcabcabab";
-		System.out.println(solution2(s));
+//		String s = "abcabcabab";
+		String s = "banana";
+		System.out.println(lengthOfLongestSubstring4(s));
 	}
 	//1차 솔루션
 	public static int solution(String s) {
@@ -50,9 +53,101 @@ public class num1 {
 				}
 			}else {
 				including[s.charAt(left)]--;
+				//이건 left를 1씩 증가시키는 것이므로 완전 최적은 아님!
 				left++;
 			}
 		}
 		return max;
 	}
+	
+	/** Approach 1: Brute Force **/
+	public int lengthOfLongestSubstring1(String s) {
+		int n = s.length();
+		int res = 0;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(checkRepetition(s, i, j)) {
+					res = Math.max(res,  j-i+1);
+				}
+			}
+		}
+		return res;
+	}
+	private boolean checkRepetition(String s, int start, int end) {
+		int[] chars = new int[128];
+		for(int i=start; i<=end;i++) {
+			char c = s.charAt(i);
+			chars[c]++;
+			if(chars[c]>1) {
+				return false;
+			}
+		}
+		return true;
+	}
+	//Time Complexity: O(n3)
+	//Space Complexity: O(min(s.length, 알파벳 수))
+	//The size of the Set is upper bounded by the size of the string Set n and the size of the charset/alphabet m
+	
+	/** Approach 2: Sliding Window **/
+	public int lengthOfLongestSubstring2(String s) {
+		int[] chars = new int[128];
+		int left=0;
+		int right=0;
+		int res=0;
+		while(right<s.length()) {
+			char r = s.charAt(right);
+			chars[r]++;
+			
+			while(chars[r]>1) {
+				char l = s.charAt(left);
+				chars[l]--;
+				left++;
+			}
+			res=Math.max(res, right-left+1);
+			right++;
+		}
+		return res;
+	}
+	//Time Complexity : O(2n)=O(n). In the worst case each character will be visited twice by i and j
+	//Space Complexity: O(min(s.length, 알파벳 수))
+	
+	/** Approach 3: Sliding Window Optimized **/
+	public int lengthOfLongestSubstring3(String s) {
+		int n = s.length(), ans = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		for(int j=0,i=0;j<n;j++) {
+			if(map.containsKey(s.charAt(j))) {
+				i=Math.max(map.get(s.charAt(j)), i);
+			}
+			ans = Math.max(ans, j-i+1);
+			map.put(s.charAt(j),j+1);
+		}
+		return ans;
+	}
+	//Time Complexity : O(n).
+	//Space Complexity:  O(min(s.length, 알파벳 수)). m is the size of the charset
+	
+	/** Approach 3: Sliding Window Optimized more space **/
+	/*int[26] for Letters 'a' - 'z' or 'A' - 'Z'
+	int[128] for ASCII
+	int[256] for Extended ASCII*/
+	public static int lengthOfLongestSubstring4(String s) {
+		Integer[] chars = new Integer[128];
+		int left = 0;
+		int right = 0;
+		int res = 0;
+		while(right < s.length()) {
+			char r = s.charAt(right);
+			Integer index = chars[r];
+			if(index!=null && index >=left && index<right) {
+				left = index+1;
+			}
+			res = Math.max(res,  right-left+1);
+			chars[r]=right;
+			right++;
+		}
+		return res;
+	}
+	//Time Complexity : O(n).
+	//Space Complexity:  O(m). m is the size of the charset
 }
---
+++ src/test/study3/num1_2.java
@@ -0,0 +1,5 @@
+package test.study3;
+
+public class num1_2 {
+
+}
