32a69de61248326b384336583855a1b46ec70ffb
seongjin_song <seongjin_song@tmax.co.kr>
2021-12-29 15:38:18 +0900

    add(study3- num4)

--- src/test/study3/num4.java
+++ src/test/study3/num4.java
@@ -1,6 +1,7 @@
 package test.study3;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class num4 {
 	//nums = {1, 3, -1, -3, 5, 3, 6, 7}
@@ -20,6 +21,9 @@ public class num4 {
 	public static void main(String[] args) {
 		int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
 		int k=3;
+		print(solution2(nums, k));
+	}
+	public static ArrayList<Integer> solution(int[] nums, int k){
 		ArrayList<Integer> ans = new ArrayList<>();
 		for(int i=0;i<nums.length-2;i++) {
 			int max = nums[i];
@@ -28,7 +32,26 @@ public class num4 {
 			}
 			ans.add(max);
 		}
-		print(ans);
+		return ans;
+	}
+	public static ArrayList<Integer> solution2(int[] nums, int k){
+		ArrayList<Integer> ans = new ArrayList<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+		int prev = Integer.MIN_VALUE;
+		for(int i=0;i<nums.length-(k-1);i++) {
+			if(i!=0) {
+				pq.remove(prev);
+				pq.add(nums[i+k-1]);
+				ans.add(pq.peek());
+			}else {
+				for(int j=i;j<i+k;j++) {
+					pq.add(nums[j]);
+				}
+				ans.add(pq.peek());
+			}
+			prev=nums[i];
+		}
+		return ans;
 	}
 	public static void print(ArrayList<Integer> list) {
 		for(int i:list)System.out.print(i+" ");
