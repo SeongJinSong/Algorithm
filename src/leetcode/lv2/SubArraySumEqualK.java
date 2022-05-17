f693497b1d2bb9bfaef8f29344834dfdc0f29f0d
seongjin_song <seongjin_song@tmax.co.kr>
2022-05-17 20:53:21 +0900

    add : SubArraySumEqualK - 성능 미통과

---
+++ src/leetcode/lv2/SubArraySumEqualK.java
@@ -0,0 +1,53 @@
+package leetcode.lv2;
+
+import java.util.Arrays;
+
+public class SubArraySumEqualK {
+    public static void main(String[] args) {
+        SubArraySumEqualK s = new SubArraySumEqualK();
+        int[] nums1 = {1, 1, 1}; int k1 =2; int result1 = 2;
+        int[] nums2 = {1, 2, 3}; int k2 =3; int result2 = 2;
+        int[] nums4 = {1, -1, 0}; int k4 = 0; int result4 = 3;
+        int[] nums5 = {-1, -1, 1}; int k5 = 0; int result5 = 1;
+        int res1 = s.subarraySum(nums1, k1);
+        int res2 = s.subarraySum(nums2, k2);
+        int res4 = s.subarraySum(nums4, k4);
+        int res5 = s.subarraySum(nums5, k5);
+        System.out.println(res1+" "+(res1==result1));
+        System.out.println(res2+" "+(res2==result2));
+        System.out.println(res4+" "+(res4==result4));
+        System.out.println(res5+" "+(res5==result5));
+    }
+    public boolean[][] map;
+    public int subarraySum(int[] nums, int k) {
+        map = new boolean[nums.length][nums.length];
+        return dfs(0, 0, nums, k);
+    }
+    public int dfs(int start, int depth, int[] nums, int k){
+        if(depth>=nums.length)return 0;
+        if(map[start][depth]){
+            return 0;
+        }
+        else{
+            map[start][depth] = true;
+        }
+        int sum = start == depth ? nums[start] : sum(start, depth, nums);
+        int result = sum==k?1:0;
+        return dfs(start,depth+1, nums, k) + dfs(start+1, depth+1, nums, k)+result;
+    }
+
+    public int dfs(int start, int depth){
+        System.out.println("## "+start+","+depth);
+        if(depth>3)return 0;
+        return dfs(start, depth + 1) + dfs(start + 1, depth + 1)+1;
+
+    }
+
+    public int sum(int start, int end, int[] nums){
+        int sum =0;
+        for(int i=start; i<=end;i++){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
