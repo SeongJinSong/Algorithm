bd7dca63dec72bb0c328c8cc11b6c7f8b29fa4f5
seongjin_song <seongjin_song@tmax.co.kr>
2022-05-30 10:12:41 +0900

    add : LongestDuplicateSubstring 브루트포스

--- src/leetcode/lv3/LongestDuplicateSubstring.java
+++ src/leetcode/lv3/LongestDuplicateSubstring.java
@@ -2,9 +2,29 @@ package leetcode.lv3;
 
 public class LongestDuplicateSubstring {
     public static void main(String[] args) {
+        LongestDuplicateSubstring lds = new LongestDuplicateSubstring();
 
+        String s1 = "banana"; //"ana"
+        if(!lds.longestDupSubstring(s1).equals("ana")){
+            System.out.println(s1 +" is wrong");
+        }
+//        String s2 = "abcd"; //""
+//        if(!lds.longestDupSubstring(s2).equals("")){
+//            System.out.println(s2 +" is wrong");
+//        }
     }
     public String longestDupSubstring(String s) {
-        return null;
+        for(int len = s.length()-1; len>=1; len--){
+            for(int i=0 ; i+len<=s.length();i++) {
+                String start = s.substring(i, i + len);
+                for (int j = i+1; j + len <= s.length(); j++) {
+                    String cur = s.substring(j, j + len);
+                    if (start.equals(cur)) {
+                        return start;
+                    }
+                }
+            }
+        }
+        return "";
     }
 }
